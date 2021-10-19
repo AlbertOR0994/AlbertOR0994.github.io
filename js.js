f59aac040819ad40f1789f28d72e370d3e838b29
AlbertOR0994 <alberto.ocete.rosario@080formacion.alzago.es>
2021-10-19 08:57:23 +0200

    eltiempo

--- js.js
+++
@@ -1,9 +0,0 @@
-const boton = document.querySelector(".boton");
-const datos = document.querySelector(".datos");
-boton.addEventListener(function(){
-    boton.addEventListener('click', function(){
-        if(datos.outerHTML){
-            boton.animate(bound);
-        }
-    })
-})
